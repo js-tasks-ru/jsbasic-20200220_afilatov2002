dc5d43c49f814862ea2a90e8bb3c276df02ab5f2
aleksey <afilatov2002@gmail.com>
2020-02-18 22:48:10 +0300

    showSalary function

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -4,5 +4,12 @@
  * @param {number} age - максимальный возраст
  * @returns {string}
  */
-function showSalary(data, age) {
+function showSalary (data, age) {
+let results = data.filter(item => item.age <= age);
+let final='';
+for (item in results) {
+final += `${results[item].name}, ${results[item].balance}\n`;
 }
+console.log(final.slice(0, -2))
+}
+
