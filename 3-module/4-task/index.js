ef056f33e339e9d1cb93dd08bb7d7a9d17bebda8
aleksey <afilatov2002@gmail.com>
2020-02-22 15:59:47 +0300

    camelize and name array functions

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -3,4 +3,21 @@
  * @returns {string[]}  объект
  */
 function namify(users) {
-}
+    let result = [];
+    for (let user of users) {
+    console.log(user.name);
+        result.push(user.name);
+    }
+    return (result);
+    }
+
+
+    // let vasya = { name: 'Вася', age: 25 };
+    // let petya = { name: 'Петя', age: 30 };
+    // let masha = { name: 'Маша', age: 28 };
+
+    // let users = [vasya, petya, masha];
+
+
+
+    // namify(users);
