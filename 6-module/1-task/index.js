0f20491515d72b2b1ce8a5a9058f5bbf7bbb3cab
aleksey <afilatov2002@gmail.com>
2020-03-04 23:44:13 +0300

    cleared table

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -16,14 +16,54 @@
 class ClearedTable {
   constructor(data) {
     this.el = document.createElement('table');
+    this.el.classList.add('pure-table');
     this.data = data;
-  }
+    this.id;
+    this.el.innerHTML = `
+                <thead>
+                <tr>
+                    <td>Name</td>
+                    <td>Age</td>
+                    <td>Salary</td>
+                    <td>City</td>
+                    <td></td>
+                </tr>
+            </thead>
+            ${data.map(
+                item => 
+                `<tr id=${item.id}>
+                <td>${item.name}</td>
+                <td>${item.age}</td>
+                <td>${item.salary}</td>
+                <td>${item.city}</td>
+                <td><a href="#delete" data-toggle-id="${item.id}">X</a></td>
+              </tr>`
+              )}
+            </table>
+    `;
+    document.onclick = (event) => {
+      let target = event.target;
+      let id = target.getAttribute('data-toggle-id');
+      if (!id) return;
+      let elem = document.getElementById(id);
+      elem.hidden = true;
+      this.id = id;
+      this.onRemoved(id);
+       };
+       
+
+   }
+   //End of constructor
+  onRemoved(id) {
+    console.log(`Из таблицы удален пользователь ${id}`);
+   }
+  
 
   /**
    * Метод который вызывается после удалении строки
    * @param {number} id - идентификатор удаляемого пользователя
    */
-  onRemoved(id) {}
+  
 }
 
 window.ClearedTable = ClearedTable;
